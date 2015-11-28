3684853667669dccd7dde955d75e482505b78127
luochenhuan <zhenhaiyucelia@gmail.com>
2015-11-28 18:01:57 -0500

    colorPicker

--- app/scripts/controllers/text_views.js
+++ app/scripts/controllers/text_views.js
@@ -4,20 +4,38 @@ angular.module('dynamoUiApp')
   .controller('TextViewsCtrl', function ($scope, $routeParams) {
     var myFirebaseRef = new Firebase('https://hackwestern.firebaseio.com/');
     myFirebaseRef = myFirebaseRef.child($routeParams['appId']).child("text_views");
+    $scope.app_name = $routeParams['appId'];
     $scope.text_views = {};
     $scope.current_text_view = {};
-      // $scope.textViewName : {
-      //  "font_color" : $scope.fontColor,
-      //  "font_size" : $scope.fontSize,
-      //  "color" : $scope.color,
-      //  "text" : $scope.text
-      // }
     $scope.text_views = {};
-
-    // $scope.text_views[$scope.textViewName]["font"]
 
     $scope.save_item = function(text_view) {
       myFirebaseRef.child($scope.current_text_view.textViewName).set(text_view);
     }
+    $scope.colorChange = function(hex) {
+	    myFirebaseRef.child($scope.current_text_view.textViewName).set({'hex':hex});
+    }
+
+    // color picker
+    var colpick = $('.demo').each(function() {
+	    $(this).minicolors({
+	      control: $(this).attr('data-control') || 'hue',
+	      inline: $(this).attr('data-inline') === 'true',
+	      letterCase: 'lowercase',
+	      opacity: false,
+	      change: function(hex, opacity) {
+	        if(!hex) return;
+	        if(opacity) hex += ', ' + opacity;
+	        try {
+	          // myFirebaseRef.child($scope.current_text_view.textViewName).set({'hex':hex});
+	          $scope.colorChange(hex);
+	          console.log(hex);
+	        } catch(e) {}
+	        $(this).select();
+	      },
+	      theme: 'bootstrap'
+	    });
+  });
+
 });
 
