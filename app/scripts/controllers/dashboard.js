d4fbf694a1ec6365f3135997bc21bdb4b6e1abd5
luochenhuan <zhenhaiyucelia@gmail.com>
2015-11-29 04:47:50 -0500

    add text view ui

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,3 +1,15 @@
 angular.module('dynamoUiApp')
-  .controller('DashboardCtrl', function () {
+  .controller('DashboardCtrl', function ($scope) {
+  	var ref = new Firebase("https://hackwestern.firebaseio.com/");
+  	$scope.apps = []
+
+	// Attach an asynchronous callback to read the data at our posts reference
+	ref.on("value", function(snapshot) {
+	  $scope.apps = Object.keys(snapshot.val());
+	  // console.log($scope.apps);
+	  $scope.$apply();
+	}, function (errorObject) {
+	  console.log("The read failed: " + errorObject.code);
+	});
+
 });
