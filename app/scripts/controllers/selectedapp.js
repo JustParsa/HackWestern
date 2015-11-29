29cc8c50870978bcd988764788f123667274e84a
Parsa Alipour <parsaalipour@hotmail.com>
2015-11-29 08:04:57 -0500

    ROUTING FULLY FUNCTIONAL BABY

--- app/scripts/controllers/selectedapp.js
+++ app/scripts/controllers/selectedapp.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('dynamoUiApp')
-  .controller('SelectedAppCtrl', function ($scope, $routeParams) {
-  	$scope.currentApp = $routeParams.appId;
+  .controller('SelectedAppCtrl', function ($scope, $routeParams, $location) {
+  	$scope.selectedApp = $routeParams.appId;
+
+  	$scope.routeToViews = function(viewName) {
+  		var newURL = "/account/" + $scope.selectedApp + "/" + viewName;
+		$location.path(newURL);
+  	};
 });
